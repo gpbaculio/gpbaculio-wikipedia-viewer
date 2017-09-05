66791824357f1c7c190291fbcfb05be095ede01c
iamglenbacs <gpbaculio@gmail.com>
2017-09-05 14:01:50 +0800

    ready to deploy

--- js/components/App.js
+++ js/components/App.js
@@ -1,93 +1,76 @@
 import React, { Component } 
-  from 'react';
-import 'jquery-ui'
-import $ from 'jquery';
+        from 'react';
+import $          from 'jquery';
 import { Button } from 'semantic-ui-react'
+import 'jquery-ui'
 
 class App extends Component {
- // initial state
- state = {
-  currentQuote: '', 
-  currentAuthor: '',
- }
- // share tweet config
- openURL = (url) => { 
-   window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
- }
 
- inIframe = () => { 
-  try { 
-    return window.self !== window.top; 
-  } catch (e) { 
-    return true; 
-  } 
- }
+state = {
+  city:'',
+  country:'',
+  temperature:'',
+  temperatureUnit:'C',
+  description:'',
+  main:'',
+  currentTempInCelsius:'',
+  icon:'',
+}
 
- getQuote = () => {  // invoke Ajax Request
- $.ajax({
-    headers: {
-      "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
-      Accept: "application/json",
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
-    success: (response) => {
-      if (typeof response === 'string') {
-       response = JSON.parse(response); 
-      }
-      this.setState(({currentAuthor, currentQuote}) => ({ 
-        currentAuthor: response.author, 
-        currentQuote: response.quote
-      }));
-    }
-  });
-}
-onTweetClick = () => {
-    if(!this.inIframe()) {
-      this.openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + this.state.currentQuote + '" ' + this.state.currentAuthor));
-    }
-}
 componentDidMount = () => {
-  $.ajax({
-    headers: {
-      "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
-      Accept: "application/json",
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
-    success: (response) => {
-      if (typeof response === 'string') {
-       response = JSON.parse(response); 
-      }
-      this.setState(({currentAuthor, currentQuote}) => ({ 
-        currentAuthor: response.author, 
-        currentQuote: response.quote
-      }));
+
+  let getWeather = (lat, lon) => {
+      console.log("getWeather lat = ", lat)
+      console.log("getWeather lon = ", lon)
+      $.ajax({
+        url: "https://fcc-weather-api.glitch.me/api/current?"+ lat + "&" + lon, 
+        success: (result) => {
+          console.log("result = ", result)
+          let currentTempInCelsius = Math.round(result.main.temp * 10) / 10; // celcius formula
+          this.setState((prevState, props) => ({
+            city: result.name.concat(" City"),
+            country: result.sys.country,
+            temperature: currentTempInCelsius+" "+ String.fromCharCode(176), // String.fromCharCode(176) is a degree icon
+            main: result.weather[0].main,
+            icon: result.weather[0].icon,
+            description: result.weather[0].description
+          }));
+          
+        }
+      });
     }
-  });
+
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+       var lat = "lat=" + position.coords.latitude;
+       var lon = "lon=" + position.coords.longitude;
+       getWeather(lat, lon);
+    });
+
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+  }
 }
+
+
+
+
+
   render() {
     return (
-      <div>
-      <span className="title"> Reactjs Random Quote Machine </span>
-        <div className="quote-box">
-            <div><span id="text">"{this.state.currentQuote}"</span>
+        <div> 
+          <span className="title"> Reactjs Show the Local Weather </span>
+            <div className="container">
+              <span style={{fontSize: '20px'}}> {`${this.state.city}, ${this.state.country}`} </span>
+              <div> <img style={{width: '100px', height: '100px'}} src={this.state.icon} /> </div>
+              <span style={{fontSize: '18px'}}> {`${this.state.main} - ${this.state.description}`}  </span>
+              <div style={{marginTop: '40px'}}>
+                <span style={{float: 'left'}}> Deployed Heroku App: <a href="https://gpbaculio-random-quote.herokuapp.com/"> link </a> </span>
+                <span style={{float: 'right'}}> Github Repo: <a href="https://github.com/iamglenbacs/gpbaculio-local-weather"> link </a> </span>
             </div>
-            <div style={{ width: '450px', height: 'auto', clear: 'both', paddingTop: '20px', fontSize: '16px', textAlign: 'right'}}>
-              - <span>{this.state.currentAuthor}</span>
             </div>
-            <div style={{marginTop: '15px'}} className="buttons">
-              <button content="Tweet" style={{float: 'left', width: '120px', height:'30px'}}  onClick={()=>this.onTweetClick()} target="_blank" > Tweet </button>
-              <button style={{float: 'right', width: '120px', height:'30px'}}  onClick={()=>this.getQuote()} >New quote</button>
-              
-            </div>
-            <div style={{marginTop: '60px'}}>
-              <span style={{float: 'left'}}> Deployed Heroku App: <a href="https://gpbaculio-random-quote.herokuapp.com/"> link </a> </span>
-              <span style={{float: 'right'}}> Github Repo: <a href="https://github.com/iamglenbacs/fcc_random_quote_machine"> link </a> </span>
-            </div>
-          </div>
-        <div className="footer"> Developed by <strong> Glendon Philipp Baculio </strong> </div>
-      </div>
+          <span className="footer"> Developed by Glendon Philipp Baculio </span>
+        </div>
     )
   }
 }
