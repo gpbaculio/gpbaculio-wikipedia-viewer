04a035783d31b10f8f0051c25f586165edf89629
iamglenbacs <gpbaculio@gmail.com>
2017-09-05 17:55:53 +0800

    ready to deploy

--- js/components/App.js
+++ js/components/App.js
@@ -4,76 +4,76 @@ import $          from 'jquery';
 import { Button } from 'semantic-ui-react'
 import 'jquery-ui'
 
-class App extends Component {
-
-state = {
-  city:'',
-  country:'',
-  temperature:'',
-  temperatureUnit:'C',
-  description:'',
-  main:'',
-  currentTempInCelsius:'',
-  currentTempInFarenheit:'',
-  icon:'',
+class Article extends Component {
+_renderSnippet = () => {
+  return { __html:this.props.article.snippet };
 }
 
-componentDidMount = () => {
+  render() {
+    return (
+        <a style={{ color: 'black', textDecoration: 'none'}} className="articleLink" href={"https://en.wikipedia.org/wiki/"+this.props.article.title} target="_blank">
+          <div style={{ borderRadius: '3px', padding: '15px', marginTop: '15px' }} className="article"> 
+            <span style={{ fontSize: '18px' }} > {this.props.article.title} </span>
+            <div style={{textAlign: 'left', fontSize: '14px', marginTop: '12px'}} dangerouslySetInnerHTML={this._renderSnippet()}/>
+          </div>
+        </a>
+      )
+  }
+}
+class App extends Component {
 
-  let getWeather = (lat, lon) => {
-      console.log("getWeather lat = ", lat)
-      console.log("getWeather lon = ", lon)
+  state = {
+    searchText:'',
+    searchResults: []
+  }
+
+  _search = (e) => {
+    // Use Ajax to handle things
+    e.preventDefault()
+    if(this.state.searchText !== '') {
+      let setState = (result) => {
+        this.setState((prevState, props) => ({
+            searchResults: result.query.search
+        }));
+      }
       $.ajax({
-        url: "https://fcc-weather-api.glitch.me/api/current?"+ lat + "&" + lon, 
-        success: (result) => {
-          console.log("result = ", result)
-          let currentTempInCelsius   = Math.round(result.main.temp * 10) / 10; // celcius formula
-          let currentTempInFarenheit = Math.round(parseInt(result.main.temp) * 9/5 + 32); // convert celcius to farenheit (formula)
-          this.setState((prevState, props) => ({
-            city: result.name.concat(" City"),
-            country: result.sys.country,
-            currentTempInCelsius: currentTempInCelsius+" "+ String.fromCharCode(176), // String.fromCharCode(176) is a degree icon
-            currentTempInFarenheit: currentTempInFarenheit+" "+ String.fromCharCode(176),
-            main: result.weather[0].main,
-            icon: result.weather[0].icon,
-            description: result.weather[0].description
-          }));
-          
+        url: 'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=' + this.state.searchText.toString(),
+        dataType: 'jsonp',
+        type: 'POST',
+        headers: {
+          'Api-User-Agent': 'Example/1.0'
+        },
+        success: function(data) {
+          console.log("data = ", data)
+          setState(data)  
         }
       });
     }
-
-  if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-       var lat = "lat=" + position.coords.latitude;
-       var lon = "lon=" + position.coords.longitude;
-       getWeather(lat, lon);
-    });
-
-  } else {
-    console.log("Geolocation is not supported by this browser.");
-  }
-}
-
-  _handleTempUnitClick = (unit) => {
-    this.setState((prevState) => ({
-      temperatureUnit: Boolean(unit === 'C') ? 'F' : 'C',
-    }));
   }
 
+  _handleInputChange = (e) => {
+    this.setState({searchText: e.target.value});
+  };
+
+  _renderArticles() {
+    return this.state.searchResults.map(searchResult =>
+      <Article
+        article={searchResult}
+      />
+    );
+  }
   render() {
     return (
         <div> 
           <span className="title"> Reactjs Show the Local Weather </span>
             <div className="container">
-              <span style={{fontSize: '20px'}}> {`${this.state.city}, ${this.state.country}`} </span>
-              <div style={{fontSize: '20px', marginTop:'10px'}}> {Boolean(this.state.temperatureUnit === 'C') ? this.state.currentTempInCelsius : this.state.currentTempInFarenheit} <span style={{cursor: 'pointer'}} className="tempUnit" onClick={() => this._handleTempUnitClick(this.state.temperatureUnit)}> {this.state.temperatureUnit} </span></div>
-              <div> <img style={{width: '100px', height: '100px'}} src={this.state.icon} /> </div>
-              <span style={{fontSize: '18px'}}> {`${this.state.main} - ${this.state.description}`}  </span>
-              <div style={{marginTop: '40px'}}>
-                <span style={{float: 'left'}}> Deployed Heroku App: <a href="https://gpbaculio-local-weather.herokuapp.com/"> link </a> </span>
-                <span style={{float: 'right'}}> Github Repo: <a href="https://github.com/iamglenbacs/gpbaculio-local-weather"> link </a> </span>
-            </div>
+              <a href="https://en.wikipedia.org/wiki/Special:Random" target="_blank">
+                <button type="button">Random Article</button>
+              </a>
+              <div style={{fontSize: '20px', marginTop:'15px'}}> 
+                <form  onSubmit={this._search}><input placeholder={'Search Wikipedia'} onChange={this._handleInputChange} /></form>
+                <button onClick={(e) => this._search(e)}> Search </button></div>
+              {this._renderArticles()}
             </div>
           <span className="footer"> Developed by Glendon Philipp Baculio </span>
         </div>
